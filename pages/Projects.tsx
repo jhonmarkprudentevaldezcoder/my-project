3002d9e8da0057ab715345103083339af675d1cf
jhon mark valdez <jhonmarkprudentevaldezcoder@gmail.com>
2023-04-09 18:34:40 +0800

    update

--- pages/Projects.tsx
+++ pages/Projects.tsx
@@ -80,11 +80,14 @@ const Projects = () => {
                 Blood bank management system
               </h3>
               <p className="bg-[#222222] text-sm md:text-base p-2 md:p-6 rounded-md xl:-ml-16">
-                Lorem Ipsum is simply dummy text of the printing and typesetting
-                industry. <span className="text-teal-400">Lorem Ipsum</span> has
-                been the industry's standard{' '}
-                <span className="text-teal-400">dummy</span> text ever since the
-                1500s,
+                <span className="text-teal-400">
+                  blood bank management system{' '}
+                </span>
+                this is what I made for those who manage those who{' '}
+                <span className="text-teal-400">request blood </span>
+                and <span className="text-teal-400">donate blood </span> here
+                they can manage their data.below you can see the materials I
+                used to build this project
               </p>
               <ul className="text-xs md:text-sm font-titleFont tracking-wide flex gap-2 md:gap-5 justify-between text-gray-400">
                 <li>VB.net</li>
