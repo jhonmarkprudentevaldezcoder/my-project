7ec1e00c62a291b6a3f978ec770caadcee7a42fa
jhon mark valdez <111213168+jhonmarkprudentevaldezcoder@users.noreply.github.com>
2023-07-06 19:15:05 +0800

    Update Datadynamix.jsx

--- components/Works/Datadynamix.jsx
+++ components/Works/Datadynamix.jsx
@@ -11,27 +11,27 @@ const Datadynamix = () => {
       className="w-full"
     >
       <h3 className="flex gap-1 font-medium text-xl font-titleFont">
-        Android App developer
+        Full stack Web developer
       </h3>
       <p className="text-sm mt-1 font-medium text-gray-400">
-        April 2023 - Present
+        April to June 2023
       </p>
       <ul className="mt-6 flex flex-col gap-3">
         <li className="text-base flex gap-2 text-gray-300">
           <span className="text-teal-400 mt-1">
             <TiArrowForward />
           </span>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s
+         Developed online platform of the technical specifications of
+          commonly procured items for DSWD Central Office.
+          The site framework used is codeigniter4 which may be utilized
+          by the end-users on downloading the specification file as attachment
+          to the procurement documents.
         </li>
         <li className="text-base flex gap-2 text-gray-300">
           <span className="text-teal-400 mt-1">
             <TiArrowForward />
           </span>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          500s
+         On-boarded as Student Trainee at Administrative Service - Building and Grounds Management Division as part of the academic requirements.
         </li>
         <li className="text-base flex gap-2 text-gray-300">
           <span className="text-teal-400 mt-1">
